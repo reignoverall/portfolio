b9219c836243ad98d09c2ee578190e94b270dd74
Samuel <u1319222@utah.edu>
2023-12-26 13:14:20 -0600

    sky rotation && plane animation integrated

---
+++ pfolio/src/models/Plane.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useRef } from "react";
+import planeScene from "../assets_3d/assets/3d/plane.glb";
+import { useAnimations, useGLTF } from "@react-three/drei";
+
+const Plane = ({ isRotating, ...props }) => {
+  const planeRef = useRef();
+  const { scene, animations } = useGLTF(planeScene);
+  const { actions } = useAnimations(animations, planeRef);
+
+  useEffect(() => {
+    actions["Take 001"].play();
+  }, [actions, isRotating]);
+
+  return (
+    <mesh {...props} ref={planeRef}>
+      <primitive object={scene} />
+    </mesh>
+  );
+};
+
+export default Plane;
