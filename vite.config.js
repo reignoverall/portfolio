7e4525cc5d9d1ccb8c4dc6473763985520063afd
Samuel <u1319222@utah.edu>
2023-12-27 22:33:19 -0600

    netlify convert

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import { qrcode } from 'vite-plugin-qrcode'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),qrcode()],
-  assetsInclude: ['**/*.glb'],base:'/portfolio/'
+  assetsInclude: ['**/*.glb'],base : './'
 })
