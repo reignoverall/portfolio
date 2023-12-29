131b6635eef095403e550d735a3035ccd503add8
Samuel <u1319222@utah.edu>
2023-12-29 11:37:57 -0600

    tensor progress

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { Home, About, Contact, Projects } from "./pages";
+import Tensor from "./pages/Tensor";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -25,6 +26,7 @@ function App() {
             />
             <Route path="contact" element={<Contact />} />
             <Route path="/about" element={<About />} />
+            <Route path="/tensor" element={<Tensor />} />
           </Routes>
         </Router>
       </main>
