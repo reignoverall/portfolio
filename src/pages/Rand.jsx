e2fbf4cf4f57e1f4731a47f64337cee166a9ea3f
Samuel <u1319222@utah.edu>
2023-12-29 19:24:17 -0600

    fixing netlify deploy

--- src/pages/Rand.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Rand = () => {
-  return <section>Hello you're a bitch ass nigga</section>;
-};
-
-export default Rand;
--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import { qrcode } from 'vite-plugin-qrcode'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),qrcode()],
-  assetsInclude: ['**/*.glb','**/*.html'],base : './'
+  assetsInclude: ['**/*.glb'],base : './'
 })
